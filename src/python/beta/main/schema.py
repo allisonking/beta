037b1064a13bdc8da5efc731c7ae8c17edc6c2b7
Allison King <allisonjuliaking@gmail.com>
2019-03-16 21:41:43 -0400

    resolve individual object

--- src/python/beta/main/schema.py
+++ src/python/beta/main/schema.py
@@ -20,6 +20,10 @@ class CommentType(DjangoObjectType):
 
 
 class Query(object):
+
+    ##########################
+    # Get all objects
+    ##########################
     all_projects = graphene.List(ProjectType)
     all_chapters = graphene.List(ChapterType)
     all_comments = graphene.List(CommentType)
@@ -33,3 +37,39 @@ class Query(object):
 
     def resolve_all_comments(self, info, **kwargs):
         return Comment.objects.select_related('chapter').all()
+
+    ##########################
+    # Get individual objects
+    ##########################
+    project = graphene.Field(
+        ProjectType, id=graphene.Int(), title=graphene.String())
+    chapter = graphene.Field(ChapterType, id=graphene.Int())
+    comment = graphene.Field(CommentType, id=graphene.Int())
+
+    def resolve_project(self, info, **kwargs):
+        id = kwargs.get('id')
+        title = kwargs.get('title')
+
+        if id is not None:
+            return Project.objects.get(pk=id)
+
+        if title is not None:
+            return Project.objects.get(title=title)
+
+        return None
+
+    def resolve_chapter(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Chapter.objects.get(pk=id)
+
+        return None
+
+    def resolve_comment(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Comment.objects.get(pk=id)
+
+        return None
