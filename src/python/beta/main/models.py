b6ba782324825b4db4df82b1b8e4e3bc80c5c951
Allison King <allisonjuliaking@gmail.com>
2019-03-17 21:39:00 -0400

    add tests for graphql mutations

--- src/python/beta/main/models.py
+++ src/python/beta/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # https://docs.djangoproject.com/en/2.1/ref/contrib/postgres/fields/#arrayfield
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
-from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -10,7 +10,7 @@ from datetime import datetime
 class Project(models.Model):
     title = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=datetime.now)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
---
+++ src/python/beta/main/tests/test_service.py
@@ -0,0 +1,91 @@
+from main.tests.gql import GraphQLTestCase
+from django.contrib.auth.models import User
+from main.models import Project
+
+
+class ProjectTestCase(GraphQLTestCase):
+    def seed_data(self):
+        user = self.seed_user()
+        Project.objects.create(title="harry potter", user=user)
+        Project.objects.create(title="casual vacancy", user=user)
+        Project.objects.create(title="cuckoo's calling", user=user)
+
+    def seed_user(self):
+        user = User.objects.create_user(username="jkrowling")
+        return user
+
+    def test_get_projects(self):
+        self.seed_data()
+        q = '''
+        { allProjects {
+            id
+            title
+            user {
+                username
+            }
+        }}
+        '''
+        resp = self.query(q)
+        data = resp['data']['allProjects']
+        # check we have the right number returned
+        self.assertEqual(len(data), 3)
+
+        # check that our titles are right
+        titles = [d['title'] for d in data]
+        self.assertEqual(
+            sorted(titles), ['casual vacancy', "cuckoo's calling", 'harry potter'])
+
+        # check that they all belong to the right user
+        for d in data:
+            self.assertEqual(d['user']['username'], 'jkrowling')
+
+    def test_delete_project(self):
+        self.seed_data()
+        q = '''
+        mutation {
+            deleteProject(projectId:3) {
+                ok
+            }
+        }
+        '''
+        count = Project.objects.count()
+        resp = self.query(q, op_name='deleteProject')
+        self.assertResponseNoErrors(resp)
+        self.assertEqual(Project.objects.count(), count-1)
+
+    def test_create_project(self):
+        self.seed_user()
+        q = '''
+        mutation {
+            createProject(title:"new project", userId:1) {
+                ok
+                project {
+                    title
+                }
+            }
+        }
+        '''
+        resp = self.query(q, op_name="createProject")
+        self.assertResponseNoErrors(resp)
+        data = resp['data']['createProject']['project']
+        self.assertEqual(Project.objects.count(), 1)
+        self.assertEqual(data['title'], 'new project')
+
+    def test_update_project(self):
+        self.seed_data()
+        project = Project.objects.first()
+        q = '''
+        mutation($projectId:Int!) {
+            updateProject(projectId:$projectId, title:"ron weasley") {
+                ok
+                project {
+                    title
+                }
+            }
+        }
+        '''
+        resp = self.query(q, op_name="updateProject",
+                          variables={'projectId': project.id})
+        self.assertResponseNoErrors(resp)
+        data = resp['data']['updateProject']['project']['title']
+        self.assertEqual(data, 'ron weasley')
